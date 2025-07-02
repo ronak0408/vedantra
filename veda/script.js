a9b2457c3d37b5cb431a82dc4bd6aa82e3a6085e
Akshay Yadav <122807716+imakshayyadavv@users.noreply.github.com>
2025-07-02 22:06:16 +0530

    Update script.js

--- veda/script.js
+++ veda/script.js
@@ -1,57 +1,20 @@
- // Vanta Background
-  VANTA.DOTS({
-    el: "#vanta-bg",
-    mouseControls: true,
-    touchControls: true,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.0,
-    scaleMobile: 1.0,
-    color: 0xf72585,
-    color2: 0x7209b7,
-    backgroundColor: 0x0a0118,
-    spacing: 30.0,
-    showLines: true
-  });
+const hamburger = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
 
-  // Remove overlay after animation
-  setTimeout(() => {
-    const overlay = document.getElementById("intro-overlay");
-    if (overlay) overlay.remove();
-  }, 6500);
+    hamburger.addEventListener('click', () => {
+      sidebar.classList.toggle('active');
+    });
 
-  const hamburger = document.getElementById('hamburger');
-const sidebar = document.getElementById('sidebar');
-const closeBtn = document.getElementById('closeBtn');
+    function toggleServices() {
+      const subServices = document.getElementById('subServices');
+      const toggleIcon = document.getElementById('toggle-icon');
+      const isVisible = subServices.style.display === 'block';
 
-hamburger.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-});
-
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('active');
-});
-
-
-
-  //  function openSidebar() {
-  //     const sidebar = document.getElementById("sidebar");
-  //     sidebar.classList.add("active");
-  //     sidebar.style.display = "block"
-  //     document.body.style.overflow = "hidden";
-  //   }
-
-  //   function closeSidebar() {
-  //     const sidebar = document.getElementById("sidebar");
-  //     sidebar.classList.remove("active");
-  //     document.body.style.overflow = "auto";
-  //     sidebar.style.display = "none"
-  //   }
-
-    function toggleDropdown() {
-      const dropdown = document.querySelector(".dropdown");
-      const arrow = document.getElementById("arrow");
-      dropdown.classList.toggle("show-dropdown");
-      arrow.classList.toggle("rotate");
-    }  
-    
+      if (isVisible) {
+        subServices.style.display = 'none';
+        toggleIcon.textContent = '+';
+      } else {
+        subServices.style.display = 'block';
+        toggleIcon.textContent = '-';
+      }
+    }
